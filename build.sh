594341c8cb26dd520147093bbf9160955b5cd7a1
Jack Christopher Gammon <spiffyRAM@gmail.com>
2021-06-26 01:50:34 +1200

    Download and decompress ubuntu image

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 echo Downloading ubuntu image
 mkdir bin
-wget -P ./bin/ https://cdimage.ubuntu.com/releases/20.04.2/release/ubuntu-20.04.2-preinstalled-server-arm64+raspi.img.xz ./bin/
+cd bin
+#wget https://cdimage.ubuntu.com/releases/20.04.2/release/ubuntu-20.04.2-preinstalled-server-arm64+raspi.img.xz
+#xz --decompress ubuntu-20.04.2-preinstalled-server-arm64+raspi.img.xz
