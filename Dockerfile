d80199fc8c87b01c0861d77d45dd230ed3ba9a49
Jack Christopher Gammon <spiffyRAM@gmail.com>
2021-06-26 20:38:35 +1200

    Commit before push

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,9 @@
 from ubuntu
+
+RUN apt-get update
+
+RUN apt-get install -y wget xz-utils
+
+COPY ./build.sh /kube-image/
+
+ENTRYPOINT ["./build.sh"]
--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
 echo Downloading ubuntu image
 mkdir bin
 cd bin
-wget https://cdimage.ubuntu.com/releases/20.04.2/release/ubuntu-20.04.2-preinstalled-server-arm64+raspi.img.xz
-xz --decompress ubuntu-20.04.2-preinstalled-server-arm64+raspi.img.xz
+wget https://cdimage.ubuntu.com/releases/20.04.2/release/ubuntu-20.04.2-preinstalled-server-arm64+raspi.img.xz > /dev/null
+xz --decompress ubuntu-20.04.2-preinstalled-server-arm64+raspi.img.xz > /dev/null
 mkdir mnt
 mount -o loop,offset=269484032 ubuntu-20.04.2-preinstalled-server-arm64+raspi.img ./mnt
 ls mnt/
-
 
 umount mnt
